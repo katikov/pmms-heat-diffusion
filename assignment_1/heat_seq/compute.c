9672b3a274c771aadcf44bb808a207a4c1ba585d
katikov <dzerzhinski02@gmail.com>
2022-02-12 16:29:14 +0100

    Revert "transpose matrix"
    This reverts commit b9083a8eec0b27dd0722c5bde516e07d5457910a.

--- assignment_1/heat_seq/compute.c
+++ assignment_1/heat_seq/compute.c
@@ -12,7 +12,7 @@ static inline void get_result(const struct parameters* p, const double* world, s
     double tmax = p->io_tmin;
     double tmin = p->io_tmax;
 
-    int N = p->M, M = p->N;
+    int N = p->N, M = p->M;
     for(int i=1;i<=N;i++){
         for(int j=1;j<=M;j++){
             double temp = world[i*(M+2)+j];
@@ -37,7 +37,7 @@ static inline void get_result(const struct parameters* p, const double* world, s
 
 static inline double get_maxdiff(const struct parameters* p, const double* world, const double* old_world){
     double maxdiff = 0;
-    int N = p->M, M = p->N;
+    int N = p->N, M = p->M;
 
     for(int i=1;i<=N;i++){
         for(int j=1;j<=M;j++){
@@ -56,7 +56,7 @@ static void draw_picture(const struct parameters* p, const double* world, size_t
     begin_picture(key, M, N, p->io_tmin, p->io_tmax);
     for(int i=1;i<=N;i++){
         for(int j=1;j<=M;j++){
-            draw_point(j-1,i-1,world[j*(N+2)+i]);
+            draw_point(j-1,i-1,world[i*(M+2)+j]);
         }
     }
     end_picture();
@@ -65,62 +65,55 @@ static void draw_picture(const struct parameters* p, const double* world, size_t
 
 void do_compute(const struct parameters* p, struct results *r)
 {
-    int N = p->N;
-    int M = p->M;
     const double weight_diag = 1/(sqrt(2)+1)/4;
     const double weight_direct = sqrt(2)/(sqrt(2)+1)/4;
     double* cur_world;
     double* next_world;
-    double* conductivity;
+    const double* conductivity = p->conductivity;
 
-    if (!(conductivity = calloc((N) * (M), sizeof(double)))) die("calloc");
-    for(int i=0;i<N;i++)for(int j=0;j<M;j++){
-        conductivity[j*N+i] = p->conductivity[i*M+j];
-    }
-
-    
+    int N = p->N;
+    int M = p->M;
     if (!(cur_world = calloc((N+2) * (M+2), sizeof(double)))) die("calloc");
     if (!(next_world = calloc((N+2) * (M+2), sizeof(double)))) die("calloc");
     
     // TODO: transpose the matrix
-    for(int j=0;j<p->M;j++){
-        cur_world[(j+1)*(p->N + 2)] = p->tinit[j];
-        next_world[(j+1)*(p->N + 2)] = p->tinit[j];
+    // for(int j=0;j<p->M;j++){
+    //     cur_world[(j+1)*(p->N + 2)] = p->tinit[j];
+    //     next_world[(j+1)*(p->N + 2)] = p->tinit[j];
 
-        cur_world[(j+2)*(p->N + 2)-1] = p->tinit[(p->N-1)*(p->M)+j];
-        next_world[(j+2)*(p->N + 2)-1] = p->tinit[(p->N-1)*(p->M)+j];
-    }
-    cur_world[0] = next_world[0] = cur_world[(p->M)*(p->N + 2)];
-    cur_world[(p->M+1)*(p->N + 2)] = next_world[(p->M+1)*(p->N + 2)] = cur_world[(p->N + 2)];
-    cur_world[p->N + 1] = next_world[p->N + 1] = cur_world[(p->M + 1)*(p->N + 2)-1];
-    cur_world[(p->M + 2)*(p->N + 2)-1] = next_world[(p->M + 2)*(p->N + 2)-1] = cur_world[2*(p->N + 2)-1];
-    for(int i=0;i<p->N;i++){
-        for(int j=0;j<p->M;j++){
-            cur_world[(j+1)*(p->N+2) + i + 1] = p->tinit[i*p->M + j];              
-        }
-    }
-
-    // for(int j=0;j<M;j++){
-    //     cur_world[j+1] = p->tinit[j];
-    //     next_world[j+1] = p->tinit[j];
-
-    //     cur_world[(N+1)*(M + 2) + j+1] = p->tinit[(N-1)*M+j];
-    //     next_world[(N+1)*(M + 2)+ j+1] = p->tinit[(N-1)*M+j];
+    //     cur_world[(j+2)*(p->N + 2)-1] = p->tinit[(p->N-1)*(p->M)+j];
+    //     next_world[(j+2)*(p->N + 2)-1] = p->tinit[(p->N-1)*(p->M)+j];
     // }
-    // cur_world[0] = next_world[0] = cur_world[M];
-    // cur_world[M + 1] = next_world[M + 1] = cur_world[1];
-    
-    // cur_world[(N+1)*(M + 2)] = next_world[(N+1)*(M + 2)] = cur_world[(N+2)*(M+2)-2];
-    // cur_world[(N+2)*(M+2)-1] = next_world[(N+2)*(M+2)-1] = cur_world[(N+1)*(M+2)+1];
-
-    // for(int i=0;i<N;i++){
-    //     for(int j=0;j<M;j++){
-    //         cur_world[(i+1)*(M+2) + j + 1] = p->tinit[i*M + j];              
+    // cur_world[0] = next_world[0] = cur_world[(p->M)*(p->N + 2)];
+    // cur_world[(p->M+1)*(p->N + 2)] = next_world[(p->M+1)*(p->N + 2)] = cur_world[(p->N + 2)];
+    // cur_world[p->N + 1] = next_world[p->N + 1] = cur_world[(p->M + 1)*(p->N + 2)-1];
+    // cur_world[(p->M + 2)*(p->N + 2)-1] = next_world[(p->M + 2)*(p->N + 2)-1] = cur_world[2*(p->N + 2)-1];
+    // for(int i=0;i<p->N;i++){
+    //     for(int j=0;j<p->M;j++){
+    //         cur_world[j*(p->N+2) + i + 1] = p->tinit[i*p->M + j];              
     //     }
     // }
+
+    for(int j=0;j<M;j++){
+        cur_world[j+1] = p->tinit[j];
+        next_world[j+1] = p->tinit[j];
+
+        cur_world[(N+1)*(M + 2) + j+1] = p->tinit[(N-1)*M+j];
+        next_world[(N+1)*(M + 2)+ j+1] = p->tinit[(N-1)*M+j];
+    }
+    cur_world[0] = next_world[0] = cur_world[M];
+    cur_world[M + 1] = next_world[M + 1] = cur_world[1];
+    
+    cur_world[(N+1)*(M + 2)] = next_world[(N+1)*(M + 2)] = cur_world[(N+2)*(M+2)-2];
+    cur_world[(N+2)*(M+2)-1] = next_world[(N+2)*(M+2)-1] = cur_world[(N+1)*(M+2)+1];
+
+    for(int i=0;i<N;i++){
+        for(int j=0;j<M;j++){
+            cur_world[(i+1)*(M+2) + j + 1] = p->tinit[i*M + j];              
+        }
+    }
     struct timespec start_time;
     clock_gettime(CLOCK_MONOTONIC, &start_time);
-    
 
     for(int iter=0; iter<p->maxiter; iter++){
         double maxdiff = get_maxdiff(p,cur_world, next_world);
@@ -133,21 +126,19 @@ void do_compute(const struct parameters* p, struct results *r)
             report_results(p,r);
         }
         for(int i=1;i<=N;i++){
-            // cur_world[i*(M+2)] = cur_world[(i+1)*(M+2)-2];
-            // cur_world[(i+1)*(M+2)-1] = cur_world[i*(M+2)+1];
-            cur_world[i] = cur_world[M*(N+2)+i];
-            cur_world[(M+1)*(N+2)+i] = cur_world[(N+2)+i];
+            cur_world[i*(M+2)] = cur_world[(i+1)*(M+2)-2];
+            cur_world[(i+1)*(M+2)-1] = cur_world[i*(M+2)+1];
         }
 
-        for(int i=1;i<=M;i++){
-            for(int j=1;j<=N;j++){
-                next_world[i*(N+2)+j] = cur_world[i*(N+2)+j] * conductivity[(i-1)*N+j-1]
-                    + (     (cur_world[(i-1)*(N+2)+j-1] + cur_world[(i-1)*(N+2)+j+1] 
-                            + cur_world[(i+1)*(N+2)+j-1] + cur_world[(i+1)*(N+2)+j+1])*weight_diag
-                        +   (cur_world[(i-1)*(N+2)+j] + cur_world[i*(N+2)+j-1]
-                            + cur_world[i*(N+2)+j+1] + cur_world[(i+1)*(N+2)+j])*weight_direct
+        for(int i=1;i<=N;i++){
+            for(int j=1;j<=M;j++){
+                next_world[i*(M+2)+j] = cur_world[i*(M+2)+j] * conductivity[(i-1)*M+j-1]
+                    + (     (cur_world[(i-1)*(M+2)+j-1] + cur_world[(i-1)*(M+2)+j+1] 
+                            + cur_world[(i+1)*(M+2)+j-1] + cur_world[(i+1)*(M+2)+j+1])*weight_diag
+                        +   (cur_world[(i-1)*(M+2)+j] + cur_world[i*(M+2)+j-1]
+                            + cur_world[i*(M+2)+j+1] + cur_world[(i+1)*(M+2)+j])*weight_direct
                     ) 
-                    * (1-conductivity[(i-1)*N+j-1]);
+                    * (1-conductivity[(i-1)*M+j-1]);
             }
         }
 
