f791b6d9ca9424e397305a000960641f0c18e66f
katikov <dzerzhinski02@gmail.com>
2022-02-13 20:26:17 +0100

    test

--- assignment_1/heat_simd/compute.c
+++ assignment_1/heat_simd/compute.c
@@ -16,7 +16,8 @@ static inline void get_result(const struct parameters* p, const double* world, s
     double tmin = p->io_tmax;
 
     int N = p->N, M = p->M;
-    int num_cols = ((M+5)/4)*4;
+    int num_cols = (M+1)/2*2+2;
+    //int num_cols = ((M+3)/4)*4+2;
     for(int i=1;i<=N;i++){
         for(int j=1;j<=M;j++){
             double temp = world[i*num_cols+j];
@@ -42,7 +43,8 @@ static inline void get_result(const struct parameters* p, const double* world, s
 
 static void draw_picture(const struct parameters* p, const double* world, size_t key){
     int N = p->N, M = p->M;
-    int num_cols = ((M+5)/4)*4;
+    //int num_cols = ((M+3)/4)*4+2;
+    int num_cols = (M+1)/2*2+2;
     begin_picture(key, M, N, p->io_tmin, p->io_tmax);
     for(int i=1;i<=N;i++){
         for(int j=1;j<=M;j++){
@@ -57,7 +59,8 @@ void do_compute(const struct parameters* p, struct results *r)
 {
     int N = p->N;
     int M = p->M;
-    int num_cols = ((M+5)/4)*4;  // M/4 upper_bound
+    //int num_cols = ((M+3)/4)*4+2;  // M/4 upper_bound
+    int num_cols = (M+1)/2*2+2;
 
     const double weight_diag_base = 1/(sqrt(2)+1)/4;
     const double weight_direct_base = sqrt(2)/(sqrt(2)+1)/4;
@@ -135,8 +138,40 @@ void do_compute(const struct parameters* p, struct results *r)
             double d0 = cur_world[(i-1)*num_cols] + cur_world[(i+1)*num_cols];
             double d1 = cur_world[(i-1)*num_cols+1] + cur_world[(i+1)*num_cols+1];
             //for(int j=1;j<=M;j++){
-            for(int j=1;j<=M;j+=4){//*(__m256d *)
+            for(int j=1;j<=M;j+=2){
+                /*
+                double d2 = cur_world[(i-1)*(M+2)+j+1] + cur_world[(i+1)*(M+2)+j+1];
+                double d3 = cur_world[(i-1)*(M+2)+j+2] + cur_world[(i+1)*(M+2)+j+2];
+                double d4 = cur_world[(i-1)*(M+2)+j+3] + cur_world[(i+1)*(M+2)+j+3];
+                double d5 = cur_world[(i-1)*(M+2)+j+4] + cur_world[(i+1)*(M+2)+j+4];
 
+
+                double temp0 = cur_world[i*(M+2)+j] * conductivity[(i-1)*M+j-1]
+                    +    (d0+d2)*weight_diag[(i-1)*M+j-1]
+                    +   (d1 + cur_world[i*(M+2)+j-1] + cur_world[i*(M+2)+j+1])*weight_direct[(i-1)*M+j-1]
+                    ;
+                double temp1 = cur_world[i*(M+2)+j+1] * conductivity[(i-1)*M+j]
+                    +    (d1+d3)*weight_diag[(i-1)*M+j]
+                    +   (d2 + cur_world[i*(M+2)+j] + cur_world[i*(M+2)+j+2])*weight_direct[(i-1)*M+j]
+                    ;
+                double temp2 = cur_world[i*(M+2)+j+2] * conductivity[(i-1)*M+j+1]
+                    +    (d2+d4)*weight_diag[(i-1)*M+j+1]
+                    +   (d3 + cur_world[i*(M+2)+j+1] + cur_world[i*(M+2)+j+3])*weight_direct[(i-1)*M+j+1]
+                    ;
+                double temp3 = cur_world[i*(M+2)+j+3] * conductivity[(i-1)*M+j+2]
+                    +    (d3+d5)*weight_diag[(i-1)*M+j+2]
+                    +   (d4 + cur_world[i*(M+2)+j+2] + cur_world[i*(M+2)+j+4])*weight_direct[(i-1)*M+j+2]
+                    ;
+                d0 = d4;
+                d1 = d5;
+
+                next_world[i*(M+2)+j] = temp0;
+                next_world[i*(M+2)+j+1] = temp1;
+                next_world[i*(M+2)+j+2] = temp2;
+                next_world[i*(M+2)+j+3] = temp3;
+            */
+           /*
+            
                 __m256d cur = _mm256_loadu_pd(&cur_world[i*num_cols+j]);
                 __m256d cond = _mm256_loadu_pd(&conductivity[(i-1)*M+j-1]);
                 __m256d sum = _mm256_mul_pd(cur,cond);
@@ -146,53 +181,104 @@ void do_compute(const struct parameters* p, struct results *r)
                 __m256d sum_ud = _mm256_add_pd(up,down);
                 __m256d left = _mm256_set_pd(sum_ud[1],sum_ud[0],d1,d0);
                 __m256d sum_diag = _mm256_add_pd(left, sum_ud);
-                sum_diag = _mm256_mul_pd(sum_diag, _mm256_loadu_pd(&weight_diag[(i-1)*M+j-1]));
+                sum = _mm256_fmadd_pd(sum_diag, _mm256_loadu_pd(&weight_diag[(i-1)*M+j-1]),sum);
 
-                sum = _mm256_add_pd(sum, sum_diag);
+                //sum = _mm256_add_pd(sum, sum_diag);
 
                 __m256d sum_direct_l = _mm256_loadu_pd(&cur_world[i*num_cols+j-1]);
                 __m256d sum_direct_r = _mm256_loadu_pd(&cur_world[i*num_cols+j+1]);
                 __m256d sum_direct_lr = _mm256_add_pd(sum_direct_l, sum_direct_r);
                 __m256d sum_direct_ud = _mm256_set_pd(sum_ud[2],sum_ud[1],sum_ud[0],d1);
                 __m256d sum_direct = _mm256_add_pd(sum_direct_lr, sum_direct_ud);
-                sum_direct = _mm256_mul_pd(sum_direct, _mm256_loadu_pd(&weight_direct[(i-1)*M+j-1]));
+                sum = _mm256_fmadd_pd(sum_direct, _mm256_loadu_pd(&weight_direct[(i-1)*M+j-1]),sum);
+                */
+               /*
+                __m256d cur = _mm256_loadu_pd(&cur_world[i*num_cols+j]);
+                __m256d cond = _mm256_loadu_pd(&conductivity[(i-1)*M+j-1]);
+                __m256d sum = _mm256_mul_pd(cur,cond);
 
-                //cur_world[i*num_cols+j] * conductivity[(i-1)*M+j-1]
-                sum = _mm256_add_pd(sum, sum_direct);
+                __m256d up = _mm256_loadu_pd(&cur_world[(i-1)*num_cols+j]);
+                __m256d down = _mm256_loadu_pd(&cur_world[(i+1)*num_cols+j]);
+                __m256d left = _mm256_loadu_pd(&cur_world[i*num_cols+j-1]);
+                __m256d right = _mm256_loadu_pd(&cur_world[i*num_cols+j+1]);
+                __m256d sum_ud = _mm256_add_pd(up,down);
+                __m256d sum_lr = _mm256_add_pd(left,right);
+                __m256d sum_direct = _mm256_add_pd(sum_lr, sum_ud);
+                sum = _mm256_fmadd_pd(sum_direct, _mm256_loadu_pd(&weight_direct[(i-1)*M+j-1]),sum);
+
+
+                __m256d ul = _mm256_loadu_pd(&cur_world[(i-1)*num_cols+j-1]);
+                __m256d ur = _mm256_loadu_pd(&cur_world[(i-1)*num_cols+j+1]);
+                __m256d dl = _mm256_loadu_pd(&cur_world[(i+1)*num_cols+j-1]);
+                __m256d dr = _mm256_loadu_pd(&cur_world[(i+1)*num_cols+j+1]);
+                __m256d sum_u = _mm256_add_pd(ul,ur);
+                __m256d sum_d = _mm256_add_pd(dl,dr);
+                __m256d sum_diag = _mm256_add_pd(sum_u, sum_d);
+                sum = _mm256_fmadd_pd(sum_diag, _mm256_loadu_pd(&weight_diag[(i-1)*M+j-1]),sum);
+
                 _mm256_storeu_pd(&next_world[i*num_cols+j],sum);
-                // for(int a=0;a<4;a++){
-                //     if(sum[a]<40){
-                //         printf("%d %d %d %lf\n",i,j,a,sum[a]);
-                //         printf("%lf %lf %lf %lf\n", sum_diag[a], sum_direct[a], cur[a], cond[a]);
-                //         die("error");
-                //     }
-                // }
-                // __m256d diff = _mm256_sub_pd(sum, cur);
-                // double absdiff = fabs(diff[0]); 
-                // maxdiff = (maxdiff>=absdiff)?maxdiff:absdiff;
-                // absdiff = fabs(diff[1]); 
-                // maxdiff = (maxdiff>=absdiff)?maxdiff:absdiff;
-                // absdiff = fabs(diff[2]); 
-                // maxdiff = (maxdiff>=absdiff)?maxdiff:absdiff;
-                // absdiff = fabs(diff[3]); 
-                // maxdiff = (maxdiff>=absdiff)?maxdiff:absdiff;
+                */
+               /*
+                __m128d cur = _mm_loadu_pd(&cur_world[i*num_cols+j]);
+                __m128d cond = _mm_loadu_pd(&conductivity[(i-1)*M+j-1]);
+                __m128d sum = _mm_mul_pd(cur,cond);
 
-                //double d3 = cur_world[(i-1)*num_cols+j+1] + cur_world[(i+1)*num_cols+j+1];
-                // double temp = cur_world[i*num_cols+j] * conductivity[(i-1)*M+j-1]
-                //     +    (d1+d3)*weight_diag[(i-1)*M+j-1]
-                //     +   (d2 + cur_world[i*num_cols+j-1] + cur_world[i*num_cols+j+1])*weight_direct[(i-1)*M+j-1]
-                //     ;
-                d0 = sum_ud[2];
-                d1 = sum_ud[3];
+                __m128d up = _mm_loadu_pd(&cur_world[(i-1)*num_cols+j]);
+                __m128d down = _mm_loadu_pd(&cur_world[(i+1)*num_cols+j]);
+                __m128d left = _mm_loadu_pd(&cur_world[i*num_cols+j-1]);
+                __m128d right = _mm_loadu_pd(&cur_world[i*num_cols+j+1]);
+                __m128d sum_ud = _mm_add_pd(up,down);
+                __m128d sum_lr = _mm_add_pd(left,right);
+                __m128d sum_direct = _mm_add_pd(sum_lr, sum_ud);
+                sum = _mm_fmadd_pd(sum_direct, _mm_loadu_pd(&weight_direct[(i-1)*M+j-1]),sum);
 
+
+                __m128d ul = _mm_loadu_pd(&cur_world[(i-1)*num_cols+j-1]);
+                __m128d ur = _mm_loadu_pd(&cur_world[(i-1)*num_cols+j+1]);
+                __m128d dl = _mm_loadu_pd(&cur_world[(i+1)*num_cols+j-1]);
+                __m128d dr = _mm_loadu_pd(&cur_world[(i+1)*num_cols+j+1]);
+                __m128d sum_u = _mm_add_pd(ul,ur);
+                __m128d sum_d = _mm_add_pd(dl,dr);
+                __m128d sum_diag = _mm_add_pd(sum_u, sum_d);
+                sum = _mm_fmadd_pd(sum_diag, _mm_loadu_pd(&weight_diag[(i-1)*M+j-1]),sum);
+*/
+                
+                __m128d cur = _mm_loadu_pd(&cur_world[i*num_cols+j]);
+                __m128d cond = _mm_loadu_pd(&conductivity[(i-1)*M+j-1]);
+                __m128d sum = _mm_mul_pd(cur,cond);
+
+                __m128d up = _mm_loadu_pd(&cur_world[(i-1)*num_cols+j+1]);
+                __m128d down = _mm_loadu_pd(&cur_world[(i+1)*num_cols+j+1]);
+                __m128d sum_ud = _mm_add_pd(up,down);
+                __m128d left = _mm_set_pd(d1,d0);
+                __m128d sum_diag = _mm_add_pd(left, sum_ud);
+                sum = _mm_fmadd_pd(sum_diag, _mm_loadu_pd(&weight_diag[(i-1)*M+j-1]),sum);
+
+                //sum = _mm256_add_pd(sum, sum_diag);
+
+                __m128d sum_direct_l = _mm_loadu_pd(&cur_world[i*num_cols+j-1]);
+                __m128d sum_direct_r = _mm_loadu_pd(&cur_world[i*num_cols+j+1]);
+                __m128d sum_direct_lr = _mm_add_pd(sum_direct_l, sum_direct_r);
+                __m128d sum_direct_ud = _mm_set_pd(sum_ud[0],d1);
+                __m128d sum_direct = _mm_add_pd(sum_direct_lr, sum_direct_ud);
+                sum = _mm_fmadd_pd(sum_direct, _mm_loadu_pd(&weight_direct[(i-1)*M+j-1]),sum);
+
+                _mm_storeu_pd(&next_world[i*num_cols+j],sum);
+
+                //d0 = sum_ud[2];
+                //d1 = sum_ud[3];
                 // double diff = (temp - cur_world[i*num_cols+j]);
                 // maxdiff = (maxdiff>diff)?maxdiff:diff;
                 // next_world[i*num_cols+j] = temp;
-            }
-            for(int j=1;j<=M;j++){
                 double diff = fabs(cur_world[(i-1)*num_cols+j]-next_world[(i-1)*num_cols+j]);
                 maxdiff = (maxdiff>=diff)?maxdiff:diff;
+                diff = fabs(cur_world[(i-1)*num_cols+j+1]-next_world[(i-1)*num_cols+j+1]);
+                maxdiff = (maxdiff>=diff)?maxdiff:diff;
             }
+            // for(int j=1;j<=M;j++){
+            //     double diff = fabs(cur_world[(i-1)*num_cols+j]-next_world[(i-1)*num_cols+j]);
+            //     maxdiff = (maxdiff>=diff)?maxdiff:diff;
+            // }
         }
 
         double* temp_world = cur_world;
