74dfa5c409bbe4314262d2336e70b715bb07aafb
katikov <dzerzhinski02@gmail.com>
2022-02-15 14:22:55 +0100

    256bit

--- assignment_1/heat_simd/compute.c
+++ assignment_1/heat_simd/compute.c
@@ -16,8 +16,8 @@ static inline void get_result(const struct parameters* p, const double* world, s
     double tmin = p->io_tmax;
 
     int N = p->N, M = p->M;
-    int num_cols = (M+1)/2*2+2;
-    //int num_cols = ((M+3)/4)*4+2;
+    //int num_cols = (M+1)/2*2+2;
+    int num_cols = ((M+3)/4)*4+2;
     for(int i=1;i<=N;i++){
         for(int j=1;j<=M;j++){
             double temp = world[i*num_cols+j];
@@ -43,8 +43,8 @@ static inline void get_result(const struct parameters* p, const double* world, s
 
 static void draw_picture(const struct parameters* p, const double* world, size_t key){
     int N = p->N, M = p->M;
-    //int num_cols = ((M+3)/4)*4+2;
-    int num_cols = (M+1)/2*2+2;
+    int num_cols = ((M+3)/4)*4+2;
+    //int num_cols = (M+1)/2*2+2;
     begin_picture(key, M, N, p->io_tmin, p->io_tmax);
     for(int i=1;i<=N;i++){
         for(int j=1;j<=M;j++){
@@ -59,8 +59,8 @@ void do_compute(const struct parameters* p, struct results *r)
 {
     int N = p->N;
     int M = p->M;
-    //int num_cols = ((M+3)/4)*4+2;  
-    int num_cols = (M+1)/2*2+2; // M/2 upper_bound
+    int num_cols = ((M+3)/4)*4+2;  
+    //int num_cols = (M+1)/2*2+2; // M/2 upper_bound
 
     const double weight_diag_base = 1/(sqrt(2)+1)/4;
     const double weight_direct_base = sqrt(2)/(sqrt(2)+1)/4;
@@ -107,6 +107,7 @@ void do_compute(const struct parameters* p, struct results *r)
     clock_gettime(CLOCK_MONOTONIC, &start_time);
 
     double maxdiff = p->threshold+1;
+    __m256d maxdiff_vec = _mm256_set1_pd(maxdiff);
     for(int iter=0; iter<p->maxiter; iter++){
         r->maxdiff = maxdiff;
         if(maxdiff < p->threshold){
@@ -127,7 +128,8 @@ void do_compute(const struct parameters* p, struct results *r)
         for(int i=1;i<=N;i++){
             double d0 = cur_world[(i-1)*num_cols] + cur_world[(i+1)*num_cols];
             double d1 = cur_world[(i-1)*num_cols+1] + cur_world[(i+1)*num_cols+1];
-            for(int j=1;j<=M;j+=2){
+            int j;
+            for(j=1;j<=M-3;j+=4){
 
                 // vectorize with flags -ftree-vectorize
                 /*
@@ -150,40 +152,49 @@ void do_compute(const struct parameters* p, struct results *r)
                 next_world[i*(M+2)+j] = temp0;
                 next_world[i*(M+2)+j+1] = temp1;
             */
-           
-                
-                __m128d cur = _mm_loadu_pd(&cur_world[i*num_cols+j]);
-                __m128d cond = _mm_loadu_pd(&conductivity[(i-1)*M+j-1]);
-                __m128d sum = _mm_mul_pd(cur,cond);
 
-                __m128d up = _mm_loadu_pd(&cur_world[(i-1)*num_cols+j+1]);
-                __m128d down = _mm_loadu_pd(&cur_world[(i+1)*num_cols+j+1]);
-                __m128d sum_ud = _mm_add_pd(up,down);
-                __m128d left = _mm_set_pd(d1,d0);
-                __m128d sum_diag = _mm_add_pd(left, sum_ud);
-                sum = _mm_fmadd_pd(sum_diag, _mm_loadu_pd(&weight_diag[(i-1)*M+j-1]),sum);
+                __m256d cur = _mm256_loadu_pd(&cur_world[i*num_cols+j]);
+                __m256d cond = _mm256_loadu_pd(&conductivity[(i-1)*M+j-1]);
+                __m256d sum = _mm256_mul_pd(cur,cond);
+
+                __m256d up = _mm256_loadu_pd(&cur_world[(i-1)*num_cols+j+1]);
+                __m256d down = _mm256_loadu_pd(&cur_world[(i+1)*num_cols+j+1]);
+                __m256d sum_ud = _mm256_add_pd(up,down);
+                __m256d left = _mm256_set_pd(sum_ud[1],sum_ud[0],d1,d0);
+                __m256d sum_diag = _mm256_add_pd(left, sum_ud);
+                sum = _mm256_fmadd_pd(sum_diag, _mm256_loadu_pd(&weight_diag[(i-1)*M+j-1]),sum);
 
                 //sum = _mm256_add_pd(sum, sum_diag);
 
-                __m128d sum_direct_l = _mm_loadu_pd(&cur_world[i*num_cols+j-1]);
-                __m128d sum_direct_r = _mm_loadu_pd(&cur_world[i*num_cols+j+1]);
-                __m128d sum_direct_lr = _mm_add_pd(sum_direct_l, sum_direct_r);
-                __m128d sum_direct_ud = _mm_set_pd(sum_ud[0],d1);
-                __m128d sum_direct = _mm_add_pd(sum_direct_lr, sum_direct_ud);
-                sum = _mm_fmadd_pd(sum_direct, _mm_loadu_pd(&weight_direct[(i-1)*M+j-1]),sum);
+                __m256d sum_direct_l = _mm256_loadu_pd(&cur_world[i*num_cols+j-1]);
+                __m256d sum_direct_r = _mm256_loadu_pd(&cur_world[i*num_cols+j+1]);
+                __m256d sum_direct_lr = _mm256_add_pd(sum_direct_l, sum_direct_r);
+                __m256d sum_direct_ud = _mm256_set_pd(sum_ud[2],sum_ud[1],sum_ud[0],d1);
+                __m256d sum_direct = _mm256_add_pd(sum_direct_lr, sum_direct_ud);
+                sum = _mm256_fmadd_pd(sum_direct, _mm256_loadu_pd(&weight_direct[(i-1)*M+j-1]),sum);
 
-                _mm_storeu_pd(&next_world[i*num_cols+j],sum);
-                d0 = sum_ud[0];
-                d1 = sum_ud[1];
+                _mm256_storeu_pd(&next_world[i*num_cols+j],sum);
+                d0 = sum_ud[2];
+                d1 = sum_ud[3];
 
-
+                __m256d diff_vec1 = _mm256_sub_pd(cur,sum);
+                __m256d diff_vec2 = _mm256_sub_pd(sum,cur);
+                __m256d diff_vec = _mm256_max_pd(diff_vec1,diff_vec2);
+                maxdiff_vec = _mm256_max_pd(maxdiff_vec, diff_vec);
+            }
+            
+            for(;j<=M;j++){
+                double d2 = cur_world[(i-1)*num_cols+j+1] + cur_world[(i+1)*num_cols+j+1];
+                double temp0 = cur_world[i*num_cols+j] * conductivity[(i-1)*M+j-1]
+                    +    (d0+d2)*weight_diag[(i-1)*M+j-1]
+                    +   (d1 + cur_world[i*num_cols+j-1] + cur_world[i*num_cols+j+1])*weight_direct[(i-1)*M+j-1]
+                    ;
+                next_world[i*num_cols+j] = temp0;
                 double diff = fabs(cur_world[(i-1)*num_cols+j]-next_world[(i-1)*num_cols+j]);
                 maxdiff = (maxdiff>=diff)?maxdiff:diff;
-                diff = fabs(cur_world[(i-1)*num_cols+j+1]-next_world[(i-1)*num_cols+j+1]);
-                maxdiff = (j<M && maxdiff>=diff)?maxdiff:diff;
-                
-
+                d0=d1;d1=d2;
             }
+            for(int k=0;k<4;k++) maxdiff = (maxdiff>=maxdiff_vec[i])?maxdiff:maxdiff_vec[i];
         }
 
         double* temp_world = cur_world;
