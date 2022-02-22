60bcea49f6bdb2661bbb73397cc8c1b1bfbc7d72
LISIMEI <lsmdlut@gmail.com>
2022-02-22 21:26:47 +0100

    vector sort test

--- assignment_2/vecsort/sequential/vecsort.c
+++ assignment_2/vecsort/sequential/vecsort.c
@@ -4,23 +4,76 @@
 #include <getopt.h>
 #include <stdio.h>
 #include <ctype.h>
-#include <omp.h>
+// #include <omp.h>
 #include <assert.h>
-
+#include <memory.h>
 /* Ordering of the vector */
-typedef enum Ordering {ASCENDING, DESCENDING, RANDOM} Order;
+typedef enum Ordering
+{
+    ASCENDING,
+    DESCENDING,
+    RANDOM
+} Order;
 
 int debug = 0;
 
-void vecsort(/* ...  */){
-    //TODO: Just Do It. Don't let your dreams be dreams.
+void top_down_mergesort(int *b, long l, int *a)
+{
+    if (l <= 1)
+        return;
+    int num_rhs = l / 2;
+    int num_lhs = l - num_rhs;
+    int *mid_a = a + num_lhs;
+    int *mid_b = b + num_lhs;
+    top_down_mergesort(a, num_lhs, b);
+    top_down_mergesort(mid_a, num_rhs, mid_b);
+    int i = 0, j = 0;
+
+    for (int k = 0; k < l; k++)
+    {
+        if (i < num_lhs && (j >= num_rhs || b[i] <= mid_b[j]))
+        {
+            a[k] = b[i];
+            i++;
+        }
+        else
+        {
+            a[k] = mid_b[j];
+            j++;
+        }
+    }
+}
+/* Sort vector v of l elements using mergesort */
+void msort(int *v, long l, int *b)
+{
+    memcpy(b, v, l * sizeof(int));
+    top_down_mergesort(b, l, v);
 }
 
-void print_v(int **vector_vectors, int *vector_lengths, long length_outer) {
+void vecsort(int **vector_vectors, int *vector_lengths, long length_outer)
+{
+    // TODO: Just Do It. Don't let your dreams be dreams.
+    for (long i = 0; i < length_outer; i++)
+    {
+        int *b = (int *)malloc(vector_lengths[i] * sizeof(int));
+        if (b == NULL)
+        {
+            fprintf(stderr, "Malloc failed...\n");
+            exit(-1);
+        }
+        msort(vector_vectors[i], vector_lengths[i], b);
+    }
+}
+
+void print_v(int **vector_vectors, int *vector_lengths, long length_outer)
+{
     printf("\n");
-    for(long i = 0; i < length_outer; i++) {
-        for (int j = 0; j < vector_lengths[i]; j++){
-            if(j != 0 && (j % 10 == 0)) {
+    for (long i = 0; i < length_outer; i++)
+    {
+        for (int j = 0; j < vector_lengths[i]; j++)
+        {
+            if (j != 0 && (j % 10 == 0))
+            {
                 printf("\n");
             }
             printf("%d ", vector_vectors[i][j]);
@@ -30,7 +83,8 @@ void print_v(int **vector_vectors, int *vector_lengths, long length_outer) {
     printf("\n");
 }
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
 
     int c;
     int seed = 42;
@@ -45,48 +99,54 @@ int main(int argc, char **argv) {
 
     struct timespec before, after;
 
-
     /* Read command-line options. */
-    while ((c = getopt(argc, argv, "adrgn:x:l:p:s:")) != -1) {
-        switch (c) {
-            case 'a':
-                order = ASCENDING;
-                break;
-            case 'd':
-                order = DESCENDING;
-                break;
-            case 'r':
-                order = RANDOM;
-                break;
-            case 'l':
-                length_outer = atol(optarg);
-                break;
-            case 'n':
-                length_inner_min = atoi(optarg);
-                break;
-            case 'x':
-                length_inner_max = atoi(optarg);
-                break;
-            case 'g':
-                debug = 1;
-                break;
-            case 's':
-                seed = atoi(optarg);
-                break;
-            case 'p':
-                num_threads = atoi(optarg);
-                break;
-            case '?':
-                if (optopt == 'l' || optopt == 's') {
-                    fprintf(stderr, "Option -%c requires an argument.\n", optopt);
-                } else if (isprint(optopt)) {
-                    fprintf(stderr, "Unknown option '-%c'.\n", optopt);
-                } else {
-                    fprintf(stderr, "Unknown option character '\\x%x'.\n", optopt);
-                }
-                return -1;
-            default:
-                return -1;
+    while ((c = getopt(argc, argv, "adrgn:x:l:p:s:")) != -1)
+    {
+        switch (c)
+        {
+        case 'a':
+            order = ASCENDING;
+            break;
+        case 'd':
+            order = DESCENDING;
+            break;
+        case 'r':
+            order = RANDOM;
+            break;
+        case 'l':
+            length_outer = atol(optarg);
+            break;
+        case 'n':
+            length_inner_min = atoi(optarg);
+            break;
+        case 'x':
+            length_inner_max = atoi(optarg);
+            break;
+        case 'g':
+            debug = 1;
+            break;
+        case 's':
+            seed = atoi(optarg);
+            break;
+        case 'p':
+            num_threads = atoi(optarg);
+            break;
+        case '?':
+            if (optopt == 'l' || optopt == 's')
+            {
+                fprintf(stderr, "Option -%c requires an argument.\n", optopt);
+            }
+            else if (isprint(optopt))
+            {
+                fprintf(stderr, "Unknown option '-%c'.\n", optopt);
+            }
+            else
+            {
+                fprintf(stderr, "Unknown option character '\\x%x'.\n", optopt);
+            }
+            return -1;
+        default:
+            return -1;
         }
     }
 
@@ -94,9 +154,10 @@ int main(int argc, char **argv) {
     srand(seed);
 
     /* Allocate vector. */
-    vector_vectors = (int **) malloc(length_outer * sizeof(int*));
-    vector_lengths = (int *) malloc(length_outer * sizeof(int));
-    if (vector_vectors == NULL || vector_lengths == NULL) {
+    vector_vectors = (int **)malloc(length_outer * sizeof(int *));
+    vector_lengths = (int *)malloc(length_outer * sizeof(int));
+    if (vector_vectors == NULL || vector_lengths == NULL)
+    {
         fprintf(stderr, "Malloc failed...\n");
         return -1;
     }
@@ -104,50 +165,56 @@ int main(int argc, char **argv) {
     assert(length_inner_min < length_inner_max);
 
     /* Determine length of inner vectors and fill them. */
-    for (long i = 0; i < length_outer; i++) {
-        int length_inner = (rand() % (length_inner_max + 1 - length_inner_min)) + length_inner_min ; //random number inclusive between min and max
-        vector_vectors[i] = (int *) malloc(length_inner * sizeof(int));
+    for (long i = 0; i < length_outer; i++)
+    {
+        int length_inner = (rand() % (length_inner_max + 1 - length_inner_min)) + length_inner_min; // random number inclusive between min and max
+        vector_vectors[i] = (int *)malloc(length_inner * sizeof(int));
         vector_lengths[i] = length_inner;
 
         /* Allocate and fill inner vector. */
-        switch (order) {
-            case ASCENDING:
-                for (long j = 0; j < length_inner; j++) {
-                    vector_vectors[i][j] = (int) j;
-                }
-                break;
-            case DESCENDING:
-                for (long j = 0; j < length_inner; j++) {
-                    vector_vectors[i][j] = (int) (length_inner - j);
-                }
-                break;
-            case RANDOM:
-                for (long j = 0; j < length_inner; j++) {
-                    vector_vectors[i][j] = rand();
-                }
-                break;
+        switch (order)
+        {
+        case ASCENDING:
+            for (long j = 0; j < length_inner; j++)
+            {
+                vector_vectors[i][j] = (int)j;
+            }
+            break;
+        case DESCENDING:
+            for (long j = 0; j < length_inner; j++)
+            {
+                vector_vectors[i][j] = (int)(length_inner - j);
+            }
+            break;
+        case RANDOM:
+            for (long j = 0; j < length_inner; j++)
+            {
+                vector_vectors[i][j] = rand();
+            }
+            break;
         }
     }
 
-    if(debug) {
+    if (debug)
+    {
         print_v(vector_vectors, vector_lengths, length_outer);
     }
 
     clock_gettime(CLOCK_MONOTONIC, &before);
 
     /* Sort */
-    vecsort(/* ... */);
+    vecsort(vector_vectors, vector_lengths, length_outer);
 
     clock_gettime(CLOCK_MONOTONIC, &after);
     double time = (double)(after.tv_sec - before.tv_sec) +
-              (double)(after.tv_nsec - before.tv_nsec) / 1e9;
+                  (double)(after.tv_nsec - before.tv_nsec) / 1e9;
 
     printf("Vecsort took: % .6e \n", time);
 
-    if(debug) {
+    if (debug)
+    {
         print_v(vector_vectors, vector_lengths, length_outer);
     }
 
     return 0;
 }
-
