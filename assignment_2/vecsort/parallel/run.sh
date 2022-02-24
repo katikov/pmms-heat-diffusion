1621a0fb76eb5cb20b5b3962b17dfcc232776ae1
PMMS course <pmms2208@fs0.cm.cluster>
2022-02-24 10:31:30 +0100

    result before discussion

--- assignment_2/vecsort/parallel/run.sh
+++ assignment_2/vecsort/parallel/run.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-array=(1 2 5 10 20 50 100 500 1000) 
+array=(1 2 5 10 20 50 100 500 1000 2000) 
 for chunk in ${array[@]}
 do
 counter=1
-    echo $chunk >> result4096.txt
+    echo $chunk >> result4096_chunk_dynamic.txt
     while [ $counter -le 3 ]
     do
-    prun -1 -np 1 ./parallel  -r -l 1000 -n 10 -x 100000 -s 42 -p 32 -c $chunk >> result4096.txt
+    prun -1 -np 1 ./parallel  -r -l 1000 -n 10 -x 100000 -s 42 -p 32 -c $chunk -t 4096 >> result4096_chunk_dynamic.txt
     ((counter++))
     done
 done
--- assignment_2/vecsort/parallel/vecsort.c
+++ assignment_2/vecsort/parallel/vecsort.c
@@ -128,7 +128,7 @@ void vecsort(int chunk_size,int **vector_vectors, int *vector_lengths, long leng
 #pragma omp parallel num_threads(outer_threads) private(b) 
     {
         b = (int*)malloc(sizeof(int)*length_inner_max);
-#pragma omp for schedule(guided,chunk_size)
+#pragma omp for schedule(dynamic,chunk_size)
         for(long i = 0; i < length_outer; i++) {
             
             //memcpy(b,vector_vectors[i],vector_lengths[i]*sizeof(int));
